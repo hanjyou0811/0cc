9db76e5f215ca5ea13a0e32379060ee394c46556
kikuepi <kikuepi@gmail.com>
2025-12-11 17:28:33 +0900

    printlnをutilsに移動した

--- utils.c
+++ utils.c
@@ -9,3 +9,11 @@ int println(const char *fmt, ...)
 	va_end(args);
 	return res;
 }
+
+int is_alnum(char c)
+{
+	return ('a' <= c && c <= 'z') || 
+		('A' <= c && c <= 'Z') ||
+		('0' <= c && c <= '9') ||
+		(c == '_');
+}
